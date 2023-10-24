2adc087f0091402e537efa215d9840d0828a15c5
Vasco Gungui <cubenda@gmail.com>
2023-10-24 11:47:37 +0100

    v-btn size - 12px

--- assets/counter-d4b3daa9.js
+++
@@ -1,1 +0,0 @@
-import{aR as e}from"./index-e5acfef5.js";const n=e({id:"counter",state:()=>({count:10}),actions:{increaseCount(){this.count++},decreaseCount(){this.count--}},getters:{oddOrEven:t=>t.count%2===0?"even":"odd"}});export{n as u};
