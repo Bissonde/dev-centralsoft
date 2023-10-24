6eda4324dbde386d507f594195a2ea166c884134
Vasco Gungui <cubenda@gmail.com>
2023-10-24 15:15:35 +0100

    topMenus - working

--- assets/tag-87548fa9.js
+++
@@ -1,1 +0,0 @@
-import{C as t}from"./index-97a3dc83.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
---
+++ assets/tag-c9d46b79.js
@@ -0,0 +1,1 @@
+import{C as t}from"./index-b176a99a.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
