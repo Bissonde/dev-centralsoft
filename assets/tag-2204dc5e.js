1770b69d83526cdc19827b521bbbeaebf285f2c9
Vasco Gungui <cubenda@gmail.com>
2023-10-24 14:36:23 +0100

    v-btn size - 15px

---
+++ assets/tag-2204dc5e.js
@@ -0,0 +1,1 @@
+import{C as t}from"./index-43574c8b.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
--- assets/tag-62e9b1dd.js
+++
@@ -1,1 +0,0 @@
-import{C as t}from"./index-08ccef6e.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
