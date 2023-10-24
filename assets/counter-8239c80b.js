7fae243d5b4ce09bc9271c95f8c1565bc2596241
Vasco Gungui <cubenda@gmail.com>
2023-10-24 14:40:38 +0100

    v-btn size - 15px

---
+++ assets/counter-8239c80b.js
@@ -0,0 +1,1 @@
+import{aR as e}from"./index-83324790.js";const n=e({id:"counter",state:()=>({count:10}),actions:{increaseCount(){this.count++},decreaseCount(){this.count--}},getters:{oddOrEven:t=>t.count%2===0?"even":"odd"}});export{n as u};
---
+++ assets/tag-bf136c78.js
@@ -0,0 +1,1 @@
+import{C as t}from"./index-83324790.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
