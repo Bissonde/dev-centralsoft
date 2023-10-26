68766764a48dab2c8ced4bfe1743f781952cd41e
Vasco Gungui <cubenda@gmail.com>
2023-10-26 18:51:34 +0100

    login page 7x

--- assets/TokenStore-1a96de4f.js
+++
@@ -1,1 +0,0 @@
-import{aD as t}from"./index-eb1bf36f.js";const n=t({id:"tokenID",state:()=>({tokenID:"null"}),actions:{setToken(e){this.tokenID=e}},getters:{getModule:e=>{e.hasChanged}}});export{n as u};
--- assets/tag-754821c9.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./index-eb1bf36f.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
