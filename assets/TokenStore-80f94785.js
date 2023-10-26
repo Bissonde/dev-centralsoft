91c2201299553b0e0e26a7102c8b3a200c91233e
Vasco Gungui <cubenda@gmail.com>
2023-10-26 15:10:51 +0100

    Login Page

---
+++ assets/TokenStore-80f94785.js
@@ -0,0 +1,1 @@
+import{aF as t}from"./index-519da4c9.js";const n=t({id:"tokenID",state:()=>({tokenID:"null"}),actions:{setToken(e){this.tokenID=e}},getters:{getModule:e=>{e.hasChanged}}});export{n as u};
---
+++ assets/counter-5790d8fd.js
@@ -0,0 +1,1 @@
+import{aF as e}from"./index-519da4c9.js";const n=e({id:"counter",state:()=>({count:10}),actions:{increaseCount(){this.count++},decreaseCount(){this.count--}},getters:{oddOrEven:t=>t.count%2===0?"even":"odd"}});export{n as u};
