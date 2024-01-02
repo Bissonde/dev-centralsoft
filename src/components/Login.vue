cca9528f66850d6afe7ddcbd4f5e4a2837c3fe22
Vasco Gungui <cubenda@gmail.com>
2024-01-02 17:27:25 +0000

    Login alert font-size

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -116,13 +116,14 @@
                     prepend-inner-icon="mdi-lock-outline" variant="outlined"
                     @click:append-inner="visible = !visible"></v-text-field>
 
+                    <v-card-text class="text-center pt-0">
                         <!-- Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password. -->
-                        <v-alert v-if="loginError" closable text="Após 3 tentativas consecutivas de login malsucedidas, sua conta será temporariamente
+                        <v-alert v-if="loginError" rounded="0" class="text-caption" closable text="Após 3 tentativas consecutivas de login malsucedidas, sua conta será temporariamente
                             bloqueada por três horas." type="error" variant="tonal">
                              <!-- <hr/> Se precisar fazer login agora, você também pode clicar em Esqueceu a senha de -->
                                     <!-- login? abaixo para redefinir a senha de login. -->
                             </v-alert>
-                   
+                   </v-card-text>
                 <!-- <v-card class="mb-4" width="200" color="surface-variant" variant="text">
                         <v-card-text style="color: white;" class="text-medium-emphasis text-caption surface-variant" color="surface-variant">
                             Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password. -->
