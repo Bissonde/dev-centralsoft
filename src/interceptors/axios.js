33ca536917eb1687215c843d50fe7defb877c518
Vasco Gungui <cubenda@gmail.com>
2024-01-03 14:04:18 +0000

    API Conn

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://rest.bissonde.ao/api/'
+axios.defaults.baseURL = 'https://bissonde.ao/api/'
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
